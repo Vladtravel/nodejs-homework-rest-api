f32cb01554ed9051452c44f4e6fe6fac7cd4dcc1
Vladyslav <vladtravel2015@gmail.com>
2021-10-06 11:10:43 +0300

    finished hw3

---
+++ model/db.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const uriDb = process.env.DB_HOST;
+
+const db = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  //   useCreateIndex: true,
+  useUnifiedTopology: true,
+  // useFindAndModify: false,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Database connection successful");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(`Database connection error db: ${err.message}`);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Have been disconnected from the Database");
+});
+
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  console.log("Database connection closed and app terminated");
+  process.exit(1);
+});
+
+module.exports = db;
